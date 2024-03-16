9c4fed88159a73aed983fba681f947d77b9b0ab8
FilipsCalijs <filipscalijs@gmail.com>
2024-03-16 17:17:39 +0200

    fix some issues

--- Password/Password/settings.py
+++ Password/Password/settings.py
@@ -117,12 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # Corrected static URL configuration
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'reactapp/build/static')
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
