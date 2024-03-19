24474a9717e3d7da6b94577376e3448236b56f8b
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-03-19 22:42:05 +0200

    add reg/login

--- Password/user/urls.py
+++ Password/user/urls.py
@@ -1,9 +1,8 @@
-
 from django.urls import path, include
-from django.views.generic import TemplateView
+from .views import authView, home
 
 urlpatterns = [
-
-    path("accounts/", include("django.contrib.auth.urls")),
-    path('', TemplateView.as_view(template_name='index.html'))
+ path("", home, name="home"),
+ path("signup/", authView, name="authView"),
+ path("accounts/", include("django.contrib.auth.urls")),
 ]
